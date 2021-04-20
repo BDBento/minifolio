e6739ba49a98f3986c314de3f8221cbea0ddb1f4
BDBento <66705552+BDBento@users.noreply.github.com>
2021-04-19 23:57:05 -0400

    novotema

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+
+
+function load_scripts(){
+    wp_enqueue_style('bootstrap-min',get_template_directory_uri().'/css/bootstrap.min.css' );
+    wp_enqueue_style('fancybox',get_template_directory_uri().'/css/jquery.fancybox.css');
+    wp_enqueue_style('main', get_template_directory_uri().'/css/main.css');
+    wp_enqueue_style('responsive',get_template_directory_uri().'/css/responsive.css');
+    wp_enqueue_style('animate',get_template_directory_uri().'/css/animate.min.css');
+    wp_enqueue_style('fontwesome','https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css');
+
+    wp_register_script('jquery-two','https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js',array(),'1.11.3',true );
+
+
+    /*wp_register_script('jquery-two', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', arrey(), '1.11.3', true);*/
+    wp_enqueue_script('bootstrap-min',get_template_directory_uri().'/js/bootstrap.min.js',array('jquery-two'),null,true);
+    wp_enqueue_script('fancyboxp-min',get_template_directory_uri().'/js/jquery.fancybox.pack.js',array('jquery-two'),null,true);
+    wp_enqueue_script('waypoints-min',get_template_directory_uri().'/js/jquery.waypoints.min.js',array('jquery-two'),null,true);
+    wp_enqueue_script('retinas-min',get_template_directory_uri().'/js/retina.min.js',array('jquery-two'),null,true);
+    wp_enqueue_script('modernizrs-min',get_template_directory_uri().'/js/modernizr.js',array('jquery-two'),null,true);
+    wp_enqueue_script('main-min',get_template_directory_uri().'/js/main.js',array('jquery-two'),null,true);
+
+
+    
+
+}
+
+add_action('wp_enqueue_scripts','load_scripts');
+
+
+/*
+add_action( 'after_setup_theme', 'twenty_twenty_one_setup' )
+*/
+
---
+++ header.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="">
+<!--<![endif]-->
+
+<head>
+<meta charset="utf-8">
+<meta name="description" content="">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Minifolio - Bootstrap Responsive Resume, Personal Portfolio Template</title>
+<?php wp_head() ?>
+</head>
+
+<body>
+<!-- header section -->
+<section class="banner" role="banner">
+  <header id="header"> 
+    <!-- navigation section  -->
+    <div class="header-content clearfix"> <a class="logo" href="#"><img src="images/logo.png" alt="Mafolio"></a>
+      <nav class="navigation" role="navigation">
+        <ul class="primary-nav">
+          <li><a href="#aboutme">About Me</a></li>
+          <li><a href="#works">Works</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
+      <a href="#" class="nav-toggle">Menu<span></span></a> </div>
+    <!-- navigation section  --> 
+  </header>
